195d0886d0c90d022191bbc964353a5a81b2479e
Michal <BlackDragon17@users.noreply.github.com>
2022-12-23 14:58:21 +0100

    Add first script utility

---
+++ Tampermonkey scripts/Remove Twitter tweet view count.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Remove Twitter tweet view count
+// @namespace    https://github.com/BlackDragon17/Utilities
+// @version      1.0
+// @description  Removes the view count next to the reply, like, and retweet buttons
+// @author       BlackDragon17
+// @match        https://*.twitter.com/*
+// @icon         https://abs.twimg.com/favicons/twitter.2.ico
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+    const styleEl = document.createElement("style");
+    const styleContent = document.createTextNode(`
+    div.css-1dbjc4n.r-1ta3fxp.r-18u37iz.r-1wtj0ep.r-1s2bzr4.r-1mdbhws > div.css-1dbjc4n.r-18u37iz.r-1h0z5md:first-child {
+        display: none;
+    }
+    `);
+    styleEl.appendChild(styleContent);
+    document.querySelector("head")?.append(styleEl);
+})();
